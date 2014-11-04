4e66cf5369986e214da5047462d942930f023247
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-05 00:13:12 +0100

    doc AFN

--- src/automates/AFN.java
+++ src/automates/AFN.java
@@ -1,24 +1,41 @@
+/**
+ * @author Matthieu Caron
+ * @author Arnaud Cojez
+ */
 package automates;
 
+/**
+ * Class defining the AFN, implementing the interface Automata
+ */
 public class AFN implements Automata {
 	
-	
+	/**
+	 * returns true if [mot] is accepted by the AFN
+	 * @return true if [mot] is accepted by the AFN
+	 */
 	@Override
 	public boolean accept(String mot) {
 		// TODO Auto-generated method stub 
 		return false;
 	}
 
+	/**
+	 * returns true if the AFN is empty
+	 * @return true if the AFN is empty
+	 */
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * returns an deterministic version of the AFN
+	 * @return the AFD based on this AFN
+	 */
 	@Override
 	public AFN deterministic() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
