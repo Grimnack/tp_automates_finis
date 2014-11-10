94d2bdd8daa7be30a32e02da241fd77d67c5e3c9
Grimnack <matthieu.caron@laposte.net>
2014-11-10 13:45:45 +0100

    Ajout de Hashcode et equals dans State.java

--- src/automates/State.java
+++ src/automates/State.java
@@ -5,24 +5,60 @@
 
 package automates;
 
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 public class State {
-	protected Map<Symbole, List<State>> lesVecteurs ;
+	protected Map<Symbole, List<State>> accessibleStates ;
 	protected String name ;
 	protected boolean isFinal ;
 	
 	
-	public State(Map<Symbole, List<State>> v, String name,boolean isFinal ){
-		this.lesVecteurs=v ;
+	public State(Map<Symbole, List<State>> a, String name,boolean isFinal ){
+		this.accessibleStates=a;
 		this.name= name ;
 		this.isFinal=isFinal ;
 	}
 	
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((accessibleStates == null) ? 0 : accessibleStates.hashCode());
+		return result;
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (accessibleStates == null) {
+			if (other.accessibleStates != null)
+				return false;
+		} else if (!accessibleStates.equals(other.accessibleStates))
+			return false;
+		return true;
+	}
+
+
+
 	/**
 	 * Permet de connaitre les prochains Etats, attention c est un AFN il y a plusieurs destinations possibles 
 	 * 
@@ -30,8 +66,6 @@ public class State {
 	 * @return les prochains états possible depuis l'Etat actuel ou null si rien n'est possible 
 	 */
 	public List<State> delta(Symbole e){
-	// a completer
-		
 	return null ;
 	}
 
