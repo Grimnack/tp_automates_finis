4d4ee358ce6d6ee77aa447a7cb0821d0dcd5b726
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-10 14:20:51 +0100

    construct tostring Epsilon

---
+++ src/automates/Epsilon.java
@@ -0,0 +1,35 @@
+/**
+ * @author Matthieu Caron
+ * @author Arnaud Cojez
+ */
+package automates;
+
+/**
+ * Class defining the Epsilon
+ */
+public class Epsilon implements Symbole {
+
+	//Attributes
+	
+	/**
+	 * Constant EPS
+	 */
+	public static final Epsilon EPS = new Epsilon();
+	
+	//Methods
+	/**
+	 * Constructor for the Epsilon class
+	 * Private because there is only one instance of Epsilon
+	 */
+	private Epsilon() {
+		
+	}
+	
+	/**
+	 * Returns a string defining the AlphaSymbol
+	 * @return a string defining the AlphaSymbol
+	 */
+	public String toString() {
+		return "Ɛ";
+	}
+}
